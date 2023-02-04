c663e9b14894343f6d0b3d19632e1c1866ac4ad5
FedeRoss01 <74314405+FedeRoss01@users.noreply.github.com>
2023-02-04 08:52:00 -0500

    Add files via upload

--- areoporto.c
+++ areoporto.c
@@ -4,24 +4,25 @@ int main() {
     time_t timet;
     struct tm *pTm;
     mkfifo(AEREO_TORRE_PATH, O_CREAT|S_IRWXU);
+    mkfifo(COMUNICAZIONE_DECOLLO, O_CREAT|S_IRWXU);
     sem_t *sem = sem_open("/np1", O_CREAT, S_IRWXU|S_IRGRP|S_IWGRP, 1);
-    sem_t*sem1 = sem_open("/np2", O_CREAT, S_IRWXU|S_IRGRP|S_IWGRP, 1);
     pid_t pid;
     pid = fork();
     if (pid < 0 ){
         fprintf(stderr,"Errore nel fork\n");
         return 1;
-    }
+    }//avvio processo hangar
     if (pid == 0 ){
         hangar ();
         time (&timet);
         pTm = localtime(&timet);
         sprintf(s, "%02d:%02d:%02d", pTm->tm_hour, pTm->tm_min, pTm->tm_sec);
         printf("%s",s);
+        sleep(1);
         printf("terminazione hangar\n");
         return 0;
     }
-    else {
+    else {//avvio processo torre
         torredicontrollo();
     }
     // attesa della terminazione dei processi hangar e torre 
@@ -34,5 +35,6 @@ int main() {
         printf("terminazione torre\n ");
     }
     unlink(AEREO_TORRE_PATH);
+    unlink(COMUNICAZIONE_DECOLLO);
     return 2;
 }
